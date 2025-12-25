e808c3af97328e7aa275c14f67cea7dd41d056e5
Felipe Ruivo <felipe.r.ruivo@gmail.com>
2025-12-25 20:04:29 -0300

    first

---
+++ first.rb
@@ -0,0 +1,13 @@
+puts "*===============================================*"
+puts "Olá bem-vindo(a) à sua biblioteca virtual!"
+puts "*===============================================*"
+print "\nPor favor, insira seu nome: "
+nome = gets.chomp
+print "\nPor favor, insira seu sobrenome: "
+sobrenome = gets.chomp
+print "\nPor favor, insira sua idade: "
+idade = gets.chomp.to_i
+print "\n\n"
+puts "*===============================================*"
+puts "Olá #{nome} #{sobrenome}, você tem #{idade} anos."
+puts "*===============================================*"
