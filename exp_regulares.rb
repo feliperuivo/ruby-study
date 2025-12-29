9c7ca32ce3c7f8c806a3438fd3253084cadef467
Felipe Ruivo <felipe.r.ruivo@gmail.com>
2025-12-29 00:13:47 -0300

    regex

--- exp_regulares.rb
+++ exp_regulares.rb
@@ -1,16 +1,8 @@
-puts "======== EXEMPLO =========\n"
-text = 'Meu WhatsApp é:'
-cel = '(99) 9 9999-9999'
-match = /\(\d{2}\) \d \d{4}-\d{4}/.match(cel)
-puts text
-puts match
-
-puts "======== COM INPUT DO USER =========\n"
 puts "Digite seu WhatsApp no formato (99) 9 9999-9999:"
-user_cel = gets.chomp
-user_match = /\(\d{2}\) \d \d{4}-\d{4}/.match(user_cel)
-if user_match
-  puts "Número VÁLIDO: #{user_match}"
+cel = gets.chomp
+match = /\(\d{2}\) \d \d{4}-\d{4}/.match(cel)
+if match
+  puts "Número VÁLIDO: #{match}"
 else
   puts "Número INVÁLIDO. Por favor, use o formato (99) 9 9999-9999."
 end
